f81c5d7fc796a33c22524cf242d9ccbeaff3c926
Fahri Dwi Sikhan <fahri.051000@gmail.com>
2026-06-30 13:04:54 +0700

    foto-frame

--- vite.config.js
+++ vite.config.js
@@ -1,45 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import jsconfigPaths from "vite-jsconfig-paths";
-import { VitePWA } from "vite-plugin-pwa";
-import react from '@vitejs/react-refresh' // atau @vitejs/plugin-
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    jsconfigPaths(), // Plugin ini yang bertugas membaca alias @ dari jsconfig.json
-    VitePWA({
-      registerType: "autoUpdate",
-      injectRegister: "auto",
-      devOptions: {
-        enabled: false,
-      },
-      workbox: {
-        sourcemap: true,
-        globPatterns: [
-          "*.{html,ico}",
-          "**/*.{js,css,woff,woff2}",
-          "**/**/**/*.{png,jpg,webp,svg}",
-        ],
-      },
-      manifest: {
-        base: "/",
-        name: "The wedding of Dilan & Mila",
-        short_name: "Dilan & Mila",
-        description: "Sabtu, 08 November 2026",
-        theme_color: "rgb(229, 229, 203)",
-        background_color: "rgb(60, 42, 33)",
-        orientation: "portrait",
-        display: "standalone",
-        icons: [
-          {
-            src: "/assets/img/favicon-logo.png",
-            sizes: "71x71",
-          }
-          // ... tambahkan sisa icon kamu di sini jika ada
-        ],
-      },
-    }),
-  ],
+  plugins: [react()],
+  // Menggunakan "/" agar folder assets terbaca dengan aman di root domain Vercel
+  base: "/", 
 });
